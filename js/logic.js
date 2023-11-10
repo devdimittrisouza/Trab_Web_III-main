d7cc7781da57dc62e4359e59a9f5556e5106d18c
WesleyDSilva <wdias00@gmail.com>
2023-11-10 01:01:27 -0300

    script

---
+++ js/logic.js
@@ -0,0 +1,14 @@
+function chamaTelaCadastro(){
+
+    window.location.href = "../html/CadastroCli.html";
+}
+
+function chamaTelaLogin(){
+
+    window.location.href = "../html/LoginCli.html";
+}
+
+function chamaTelaMenu() {
+
+    window.location.href = "../html/Menu.html"
+}
