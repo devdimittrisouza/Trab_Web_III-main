7240737906ddc34eddd1fe371ad27c4accded4b5
Rafael <rafadograu11@hotmail.com>
2023-11-10 12:56:39 -0300

    Adiçao de conexoes

--- js/logic.js
+++ js/logic.js
@@ -1,30 +1,30 @@
 function chamaTelaCadastro(){
 
-    window.location.href = "../html/CadastroCli.html"
+    window.location.href = "CadastroCli.html"
 }
 
 function chamaTelaLogin(){
 
-    window.location.href = "../html/LoginCli.html"
+    window.location.href = "LoginCli.html"
 }
 
 function chamaTelaMenu() {
 
-    window.location.href = "../html/Menu.html"
+    window.location.href = "Menu.html"
 }
 
 function chamaTelaPagamento() {
 
-    window.location.href = "../html/Pagamento.html"
+    window.location.href = "Pagamento.html"
 }
 
 function chamaTelaPreparacao() {
 
     console.log("chamou a tela de preparação");
-    window.location.href = "../html/TelaPreparacao.html";
+    window.location.href = "TelaPreparacao.html";
 }
 
 function chamaTelaEntrega(){
 
-    window.location.href = "../html/entrega.html"
+    window.location.href = "entrega.html"
 }
